d593e07d704b181feb138859bd27cef7eaf0f947
Sarvesh Patkar <118739592+Emperor2810@users.noreply.github.com>
2024-02-16 14:00:03 +0530

    added JS

---
+++ task1.js
@@ -0,0 +1,8 @@
+function showsidebar(){
+    const sidebar = document.querySelector('.sidebar-content')
+    sidebar.style.display = 'flex'
+}
+function closesidebar(){
+    const sidebar = document.querySelector('.sidebar-content')
+    sidebar.style.display = 'none'
+}
